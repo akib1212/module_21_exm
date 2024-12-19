cf93842dc57ec05555e36c2ea5ce8bc42eb97046
Akib Mahamud <akibmahamud55@gmail.com>
2024-12-19 21:48:18 +0600

    03

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,32 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'your_jwt_secret';
+const User = require('../models/User');
 
-exports.authenticate = (req, res, next) => {
-    const token = req.cookies.token;
+const JWT_SECRET = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-
+exports.register = async (req, res) => {
     try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        req.userId = decoded.userId;
-        next();
+        const { firstName, lastName, NIDNumber, phoneNumber, password, bloodGroup } = req.body;
+        const user = new User({ firstName, lastName, NIDNumber, phoneNumber, password, bloodGroup });
+        await user.save();
+        res.status(201).json({ message: 'User registered successfully' });
     } catch (err) {
-        res.status(401).json({ message: 'Invalid token' });
+        res.status(400).json({ error: err.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { phoneNumber, password } = req.body;
+        const user = await User.findOne({ phoneNumber });
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
+        res.cookie('token', token, { httpOnly: true });
+        res.json({ message: 'Login successful' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+exports.authenticate = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
